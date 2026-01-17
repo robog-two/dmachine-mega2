9ad67718b6b8ca45f4aed469c67512dec04b70d6
Samuel Knight <robogamingbysam@icloud.com>
2026-01-16 19:28:00 -0600

    Checkout proper branch

--- initialize.sh
+++ initialize.sh
@@ -33,7 +33,7 @@ docker --version
 
 git clone https://github.com/robog-two/grapevine.git /tmp/grapevine
 
-(cd /tmp/grapevine; docker compose up --build &)
+(cd /tmp/grapevine; git checkout genai-here-only; docker compose up --build &)
 
 echo "=== One-time initialization complete ==="
 
