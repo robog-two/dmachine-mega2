487e6332bb4ff0d77d7d4384588eca0c7aef88cc
Samuel Knight <robogamingbysam@icloud.com>
2026-01-16 18:30:24 -0600

    Remove unecessary sudo

--- initialize.sh
+++ initialize.sh
@@ -6,27 +6,27 @@ echo "=== Starting one-time system initialization ==="
 # Install docker, setup, & run the Grapevine indexer
 
 # Update the apt package index
-sudo apt-get update
+apt-get update
 
 # Install packages to allow apt to use a repository over HTTPS
-sudo apt-get install -y \
+apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
 # Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Set up the stable repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
   https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Refresh the package index and install Docker Engine
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Verify the installation
 docker --version
